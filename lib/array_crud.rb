a22c1e4da60cbfbdc9bad5059a2805c5f12f4c1b
Nicholas Dongoske <nickdongoske@gmail.com>
2019-12-26 00:24:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,6 +34,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+  retrieve_element_from_index = array
+  retrieve_element_from_index.index(-1)
   
 end
 
